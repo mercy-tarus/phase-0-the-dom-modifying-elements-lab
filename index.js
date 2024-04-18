bf96a219091ac2b284f0a1da1640e4252125a20b
mercy-tarus <merciechelah1@gmail.com>
2024-04-18 19:01:54 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+/** @format */
+
 // Write your code here!
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+const yourName = "John";
+
+newHeader.innerHTML = `${yourName} is the champion`;
+
+document.body.appendChild(newHeader);
